301c04e13c15d8fa67c71d8ec68aaaf65d13cb32
Horton Lai <hortonht.lai@gmail.com>
2023-01-14 16:20:38 -0800

    Implement colour change on mouse trail

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-const gridContainer = document.querySelector('.grid-container');
+main();
 
-let total = 256;
+function main() {
+    const gridContainer = document.querySelector('.grid-container');
 
-for (let i = 0; i < total; i++) {
-    let gridItem = document.createElement('div');
-    gridItem.classList.add('grid-item');
-    gridItem.textContent = i;
-    gridContainer.appendChild(gridItem);
+    let total = 256;
+
+    for (let i = 0; i < total; i++) {
+        let gridItem = document.createElement('div');
+        gridItem.classList.add('grid-item');
+        gridItem.classList.add(i);
+        gridItem.textContent = i;
+        gridItem.addEventListener('mouseenter', leaveTrail);
+        gridContainer.appendChild(gridItem);
+    }
+
+    let gridTemplateColumns = '';
+
+    for (let i = 0; i < Math.sqrt(total); i++) {
+        gridTemplateColumns = gridTemplateColumns + 'min-content ';
+    }
+
+    gridContainer.style.gridTemplateColumns = gridTemplateColumns;
 }
 
-let gridTemplateColumns = '';
-
-for (let i = 0; i < Math.sqrt(total); i++) {
-    gridTemplateColumns = gridTemplateColumns + 'min-content ';
+function leaveTrail(e) {
+    e.target.style.backgroundColor = 'pink';
 }
-
-gridContainer.style.gridTemplateColumns = gridTemplateColumns;
